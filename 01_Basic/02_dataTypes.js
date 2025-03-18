0dcacc45a1decc0c5a0debbc4acc40c8308d4d1b
Lokesh Patil <114599734+logicallk@users.noreply.github.com>
2025-03-18 10:39:20 +0000

    dataTpes and conversions operation study

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" ; //treat all js code as a newer version
+
+//alert(2+5) //we are using nodeJs  ,not browser
+
+let name="Lokesh"
+let age=22
+let isMarried=false
+let state;
+
+//string => ""
+//number=>2 to power of 53
+//bigint
+//boolean => true/false
+//null =>standalone value  and its type is object
+//undefined 
+//symbol =>unique
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
+
+
---
+++ 01_Basic/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let score="33as"
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+/*let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber) //output is NaN not a number  */
+
+//"33" => 33
+// "33asd" => NaN  (Not a Number)
+// true => 1; false => 0
+
+
+let isLoggedIn= "Lokesh"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// "lokesh" => true
+// 0 =>false ; 1 =>true
+// 2342 => true
